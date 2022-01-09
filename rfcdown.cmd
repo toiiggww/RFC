a8bdfc919f02399d23d05f1ab188f336af7002c6
繧九醉 <toiiggww@163.com>
2022-01-10 00:17:50 +0800

    Update 5 : RFC updated to 9170

--- rfcdown.cmd
+++ rfcdown.cmd
@@ -1,1 +1,18 @@
-for /l %%i in (%1,1,%2) do curl -O http://www.rfc-editor.org/rfc/rfc%%i.txt
+@echo off
+rem this file used for download rfc text file from rfc-editor.org
+
+if [%1] equ [] goto :usage
+if [%2] equ [] goto :usage
+if exist download rd /s /q download
+md download
+
+for /l %%i in (%1,1,%2) do curl -s -o download\rfc%%i.txt https://www.rfc-editor.org/rfc/rfc%%i.txt & echo download rfc%%i.txt
+explorer download
+goto :eof
+
+:usage
+echo %0 [start] [end]
+echo [start] will be the rfc number for start to download
+echo [end] will be the rfc number for end of download
+
+:eof
